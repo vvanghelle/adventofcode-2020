abaef6b02b56fbe88c1b04bab96cd817e9b7c2de
Vincent Vanghelle <Vincent.Vanghelle@freschelegacy.com>
2020-12-02 12:32:15 -0500

    Fix Puzzle reader for fat jars

--- src/main/java/com/vg/adventofcode/year2020/lib/PuzzleReader.java
+++ src/main/java/com/vg/adventofcode/year2020/lib/PuzzleReader.java
@@ -1,21 +1,32 @@
 package com.vg.adventofcode.year2020.lib;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PuzzleReader {
 
-    public Stream<String> readInput(String filename) throws IOException, URISyntaxException, NullPointerException {
-        ClassLoader cl = getClass().getClassLoader();
-        URL resource = cl.getResource(filename);
-        URI uri = resource.toURI();
-        Path path = Paths.get(uri);
-        return Files.lines(path);
+    public Stream<String> readInput(String filename) throws IOException, NullPointerException {
+
+        InputStream inputStream = PuzzleReader.class.getClassLoader().getResourceAsStream(filename);
+        if (inputStream != null) { // this means you are inside a fat-jar / container
+            try (
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+            ) {
+                List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+                return lines.stream();
+            }
+        } else {
+            String path = this.getClass().getResource(filename).getPath();
+            return Files.lines(Paths.get(path));
+        }
+
     }
 }
