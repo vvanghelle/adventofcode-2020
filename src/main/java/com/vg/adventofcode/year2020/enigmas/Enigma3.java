2035a0ebd604a56984769789cf4cc5c2a4aae835
Vincent Vanghelle <vincent.vanghelle@zenika.com>
2020-12-03 00:30:37 -0500

    Implement day 3

--- src/main/java/com/vg/adventofcode/year2020/enigmas/Enigma3.java
+++ src/main/java/com/vg/adventofcode/year2020/enigmas/Enigma3.java
@@ -3,6 +3,8 @@ package com.vg.adventofcode.year2020.enigmas;
 import com.vg.adventofcode.year2020.utils.LogExecutionTime;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -14,13 +16,61 @@ public class Enigma3 extends AbstractEnigma {
     @Override
     @LogExecutionTime
     public String computePart1(Stream<String> inputs) {
-        return "todo";
+        List<String> rows = inputs.collect(Collectors.toList());
+        return String.valueOf(countTreesWithSlope(rows, 3, 1));
     }
 
     @Override
     @LogExecutionTime
     public String computePart2(Stream<String> inputs) {
-        return "todo";
+        List<String> rows = inputs.collect(Collectors.toList());
+        int[][] slopes = new int[][]{
+                new int[]{1, 1},
+                new int[]{3, 1},
+                new int[]{5, 1},
+                new int[]{7, 1},
+                new int[]{1, 2}};
+
+        long currentMultiplicationResult = 1;
+
+        for (int[] slope : slopes){
+            currentMultiplicationResult = currentMultiplicationResult * countTreesWithSlope(rows, slope[0], slope[1]);
+        }
+        return String.valueOf(currentMultiplicationResult);
+    }
+
+
+    /** Scheme (repetitive on the right)
+
+     ..##.......
+     #...#...#..
+     .#....#..#.
+     ..#.#...#.#
+     .#...##..#.
+     ..#.##.....
+     .#.#.#....#
+     .#........#
+     #.##...#...
+     #...##....#
+     .#..#...#.#
+     */
+    private int countTreesWithSlope(List<String> rows, int right, int down) {
+        int i =0, j=0, nbTrees = 0;
+        while (j < rows.size())
+        {
+            // j = row;
+            // i = column;
+            String row = rows.get(j);
+            // repeat scheme on right : modulo
+            char currentChar = row.charAt(i % row.length());
+            if (currentChar == '#')
+                nbTrees++;
+
+            j += down;
+            i += right;
+        }
+
+        return nbTrees;
     }
 
     @Override
--- src/test/java/com/vg/adventofcode/year2020/enigmas/Enigma3Test.java
+++ src/test/java/com/vg/adventofcode/year2020/enigmas/Enigma3Test.java
@@ -19,10 +19,38 @@ public class Enigma3Test {
 
     @Test
     public void computePart1_withEntryList() {
-        Stream<String> inputs = Stream.of("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc");
+        Stream<String> inputs = Stream.of("..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#");
 
         String result = day3.computePart1(inputs);
 
-        assertThat(result).isNotNull().isEqualTo("todo");
+        assertThat(result).isNotNull().isEqualTo("7");
+    }
+    @Test
+    public void computePart2_withEntryList() {
+        Stream<String> inputs = Stream.of("..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#");
+
+        String result = day3.computePart2(inputs);
+
+        assertThat(result).isNotNull().isEqualTo("336");
     }
 }
